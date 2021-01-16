28c5088ec59074762cfaa66652c1416c76d3d5d3
Suril <suril.p@somaiya.edu>
2021-01-16 14:50:36 +0530

    Basic Angular Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
